628911149d4bfc466a545d528a9d50e7ef812f4f
civic86 <kaikoharazawa@gmail.com>
2024-02-10 17:18:02 -0800

    Designed main page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d75aa639cd5c115b"; }
+/******/ 		__webpack_require__.h = function() { return "37de3e5396df942e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,42 @@
-import { Heading } from "@chakra-ui/react";
+import NavBar from "./components/navBar";
+import CaptionCarousel from "./components/captionCarousel";
+import NavBar2 from "./components/navBar2";
+import { Box, Center, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import Card from "./components/card";
 
-function HomePage() {
-  return <Heading>Welcome to Next.js!</Heading>;
+const cardsData = [
+  { id: "1", title: "Title 1", language: "Language 1", description: "Description 1", imageSrc: "image1.jpg" },
+  { id: "2", title: "Title 2", language: "Language 2", description: "Description 2", imageSrc: "image2.jpg" },
+  { id: "3", title: "Title 3", language: "Language 3", description: "Description 3", imageSrc: "image3.jpg" },
+  { id: "4", title: "Title 4", language: "Language 4", description: "Description 4", imageSrc: "image4.jpg" },
+  { id: "5", title: "Title 5", language: "Language 5", description: "Description 5", imageSrc: "image5.jpg" },
+  { id: "6", title: "Title 6", language: "Language 6", description: "Description 6", imageSrc: "image6.jpg" },
+]
+
+export default function Index(): JSX.Element {
+  return (
+    <>
+      <NavBar2 />
+      <NavBar />
+      <CaptionCarousel/>
+
+      <Box mt={10}>
+        <Text textAlign='center' fontSize={"xx-large"}>Products</Text>
+        <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }} gap={-5}>
+          {cardsData.map(card => (
+            <GridItem key={card.id} w='100%' h='auto'>
+              <Card
+                id={card.id}
+                title={card.title}
+                language={card.language}
+                description={card.description}
+                imageSrc={card.imageSrc}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
+
+    </>
+  );
 }
-
-export default HomePage;
